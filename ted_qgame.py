bdce628a2d97cb5ed3ab62f8527dacdb1cd302bb
yasasp <33986971+yasasp@users.noreply.github.com>
2019-02-02 18:50:53 -0800

    Update ted_qgame.py

--- ted_qgame.py
+++ ted_qgame.py
@@ -2,7 +2,7 @@
 # TED Talk Video https://www.youtube.com/watch?v=QuR969uMICM
 # This Code is describing a coin flip game explained on Ted Talk, where I try to implement the game in D-Wave.
 
-# If I want to check, whether QC cheated or not, I have to go back to and check the coin state of the
+# If I want to check, whether QC cheated or not, I have to go back and check the coin state of the
 # intermediate steps. To do this I have to ask QC. So, QC will 'lie" (possibly) as it will only show
 # the coin states which lead to a win. Isn't this cheating ? Can anyone correct if I am wrong?
 
